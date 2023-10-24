680c97e08a2700856bed90279a91ddba44fbd43e
Viktor Gorbach <viktor.gorbatch@yandex.ru>
2023-10-24 07:38:30 +0300

    models DONE

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+
+class Title(models.Model):
+    text = models.TextField()
+    year = models.IntegerField()
+    description = models.TextField()
+    genre = models.ForeignKey(
+        Genre, on_delete=models.SET_NULL, null=True, default=None
+    )
+    category = models.OneToOneField(
+        Category, on_delete=models.SET_NULL, null=True, default=None
+    )
+    
