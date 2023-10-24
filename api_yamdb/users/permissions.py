4fd69170ebce5f3942b768d4c89b22dae5717de2
Alexey Komissarov <al-ar-ol@yandex.ru>
2023-10-25 01:50:50 +0300

    pass 33 test

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -8,4 +8,5 @@ class IsOwnerOrAdminOnly(permissions.BasePermission):
 
 class AdminOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == 'admin'
+        return request.user.is_authenticated and (
+            request.user.role == 'admin' or request.user.is_superuser)
