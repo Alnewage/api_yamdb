f66463d7645cbdae3e806e67fd2b9178f8bc66a0
Alexey Komissarov <al-ar-ol@yandex.ru>
2023-10-24 23:19:53 +0300

    noname1

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -4,7 +4,18 @@ from django.core.exceptions import ValidationError
 from django.db import models
 
 
+def username_validator(value):
+    if value == "me":
+        raise ValidationError(
+            {"username": "Username 'me' is not allowed."})
+
+
 class MyUser(AbstractUser):
+    username = models.CharField(
+        'Имя пользователя', max_length=150, unique=True, validators=[
+            username_validator
+        ]
+    )
     email = models.EmailField('Email', max_length=128, unique=True, )
     password = models.CharField('Пароль', max_length=128, blank=True,
                                 null=True)
@@ -12,20 +23,6 @@ class MyUser(AbstractUser):
         'Роль', choices=settings.ROLE_CHOICES, default='user', max_length=9, )
     bio = models.TextField('Биография', blank=True)
     confirmation_code = models.CharField(max_length=32)
-
-    # objects = CustomUserManager()
-
-    def clean(self):
-        super().clean()  # Вызываем базовую валидацию
-
-        # Дополнительная валидация
-        if self.username == "me":
-            raise ValidationError(
-                {"username": "Username 'me' is not allowed."})
-
-    def save(self, *args, **kwargs):
-        self.full_clean()  # Вызываем валидацию перед сохранением
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.username
