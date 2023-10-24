54c685c639e1e7f4c51471058f7103da9da927eb
Alexey Komissarov <al-ar-ol@yandex.ru>
2023-10-24 22:50:57 +0300

    add urls

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -2,15 +2,15 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-# from .views import (RegistrationViewSet, TokenViewSet, UserProfileUpdateView,
-#                     UserProfileViewSet)
+from .views import (RegistrationViewSet, TokenViewSet, UserProfileUpdateView,
+                    UserProfileViewSet)
 
 router = DefaultRouter()
-# router.register(r'auth', TokenViewSet, basename='token')
-# router.register(r'auth/signup', RegistrationViewSet, basename='registration')
-# router.register(r'users', UserProfileViewSet, basename='user')
+router.register(r'auth', TokenViewSet, basename='token')
+router.register(r'auth/signup', RegistrationViewSet, basename='registration')
+router.register(r'users', UserProfileViewSet, basename='user')
 
 urlpatterns = [
     path('', include(router.urls)),
-    # path('users/me/', UserProfileUpdateView.as_view(), name='me'),
+    path('users/me/', UserProfileUpdateView.as_view(), name='me'),
 ]
