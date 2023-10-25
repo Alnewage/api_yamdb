fba588163f0d1ac6c58b5dc7b59508f24821335f
Alexey Komissarov <al-ar-ol@yandex.ru>
2023-10-25 13:13:15 +0300

    optomize 3

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -2,9 +2,21 @@ import re
 
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
+
 from rest_framework import serializers
 
 User = get_user_model()
+
+
+class ValidateUsernameMixin:
+    def validate_username(self, value):
+        if not re.match(r'^[\w.@+-]+$', value):
+            raise serializers.ValidationError(
+                "Username must contain only letters, numbers,"
+                " and characters .@+-")
+        if value == 'me':
+            raise ValidationError("Username 'me' is not allowed.")
+        return value
 
 
 class TokenSerializer(serializers.Serializer):
@@ -12,53 +24,42 @@ class TokenSerializer(serializers.Serializer):
     confirmation_code = serializers.CharField()
 
 
-class RegistrationSerializer(serializers.Serializer):
+class RegistrationSerializer(ValidateUsernameMixin, serializers.Serializer):
     username = serializers.CharField(max_length=150, required=True)
     email = serializers.EmailField(max_length=254, required=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'bio',)
+        fields = (
+            'username', 'email', 'first_name', 'last_name', 'bio', 'role',)
 
     def validate(self, data):
         username = data.get('username')
         email = data.get('email')
-        if not re.match(r'^[\w.@+-]+$', username):
-            raise serializers.ValidationError(
-                "Username must contain only letters,"
-                " numbers, and characters .@+-")
-        if username == 'me':
-            raise ValidationError(
-                {"username": "Username 'me' is not allowed."})
+
         try:
             user = User.objects.get(username=username)
         except User.DoesNotExist:
             user = None
+
         if User.objects.filter(email=email).exists() and not user:
             raise ValidationError({"email": "Email already exists."})
         if user and user.email != email:
             raise ValidationError({"email": "Email incorrect."})
+
         return data
 
 
-class UserProfileSerializer(serializers.ModelSerializer):
+class UserProfileSerializer(ValidateUsernameMixin,
+                            serializers.ModelSerializer):
     class Meta:
         model = User
         fields = (
             'username', 'email', 'first_name', 'last_name', 'bio', 'role',)
-        # read_only_fields = ('role',)
-
-    def validate_username(self, value):
-        if not re.match(r'^[\w.@+-]+$', value):
-            raise serializers.ValidationError(
-                "Username must contain only letters,"
-                " numbers, and characters .@+-")
-        return value
 
     def validate_role(self, value):
         user = self.context['request'].user
         if user.role == 'admin' or user.is_superuser:
             return value
         raise serializers.ValidationError(
-            "You don't have permission to change the role of this user."
-        )
+            "You don't have permission to change the role of this user.")
