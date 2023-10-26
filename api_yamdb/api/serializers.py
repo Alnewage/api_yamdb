c466e229a0154017351b15c1e708d1b92657e971
Alexey Komissarov <al-ar-ol@yandex.ru>
2023-10-26 09:33:28 +0300

    pass 52 tests

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,39 @@
+from reviews.models import Title, Category, Genre
+from rest_framework import serializers
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ('name', 'slug')
+        model = Category
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ('name', 'slug')
+        model = Genre
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(
+        slug_field='name', queryset=Category.objects.all()
+    )
+    genre = serializers.SlugRelatedField(
+        slug_field='name', queryset=Genre.objects.all(), many=True
+    )
+    rating = serializers.SerializerMethodField()
+
+    class Meta:
+        fields = ('name', 'year', 'rating', 'description', 'category', 'genre')
+        model = Title
+        read_only_field = 'rating'
+
+    def get_rating(self, obj):
+        titles = Genre.objects.all()
+        x = 0
+        for object in titles:
+            x += object.id
+        x = x / titles.count()
+        return x
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import mixins, permissions, viewsets, filters
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, \
+    ListCreateAPIView, DestroyAPIView
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from api.serializers import (CategorySerializer, GenreSerializer,
+                             TitleSerializer)
+from reviews.models import Category, Genre, Title
+from users.permissions import AdminOnly
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    permission_classes = (permissions.AllowAny, )
+    http_method_names = ['get', 'post', 'patch', 'delete', ]
+
+
+class CategoryViewSet(ListCreateAPIView, DestroyAPIView, GenericViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+
+    def get_permissions(self):
+        if self.request.method in permissions.SAFE_METHODS:
+            return (permissions.AllowAny(), )
+        return (AdminOnly(), )
+
+
+class GenreViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (permissions.AllowAny, )
