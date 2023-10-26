0b9ad23300d00551bdfb83c33bf38681340a3f8b
Alexey Komissarov <al-ar-ol@yandex.ru>
2023-10-26 08:34:54 +0300

    add comma in admin.py

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -4,6 +4,6 @@ from django.contrib.auth.admin import UserAdmin
 from .models import MyUser
 
 UserAdmin.fieldsets += (
-    ('Extra Fields', {'fields': ('role', 'bio',)})
+    ('Extra Fields', {'fields': ('role', 'bio',)}),
 )
 admin.site.register(MyUser, UserAdmin)
