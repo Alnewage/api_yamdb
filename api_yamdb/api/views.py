aaabb86d0770328025b3618ed74ed7812ad37bec
ViktorGorbach <128081583+ViktorGorbach@users.noreply.github.com>
2023-10-26 10:07:14 +0300

    Feature/tit cat gen (#3)
    * models DONE
    
    * db done
    
    * optimized
    
    * optimized 2
    
    * dumpdata db.json

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from api.serializers import (
+    TitleSerializer, CategorySerializer, GenreSerializer,
+)
+from rest_framework import viewsets, permissions
+from reviews.models import Title, Category, Genre
+from rest_framework import mixins
 
-# Create your views here.
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    permission_classes = (permissions.AllowAny, )
+    http_method_names = ['get', 'post', 'patch', 'delete', ]
+
+
+class CategoryViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (permissions.AllowAny, )
+
+
+class GenreViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (permissions.AllowAny, )
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True, max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True, max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.IntegerField()
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, default=None
+    )
+    description = models.TextField(null=True, blank=True)
+    genre = models.ManyToManyField(Genre)
+
+    def __str__(self) -> str:
+        return self.name
