61c2f3b112a3fa459a99b63111484ecc162593f1
Alexey Komissarov <al-ar-ol@yandex.ru>
2023-10-27 18:05:58 +0300

    optimize 3

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,64 +1,65 @@
 from django.db.models import Avg
 from rest_framework import serializers
 
-from reviews.models import Category, Genre, TitleGenre, Title, Review, Comment
+from reviews.models import Category, Comment, Genre, Review, Title, TitleGenre
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Category."""
+
     class Meta:
         fields = ('name', 'slug')
         model = Category
 
 
 class GenreSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Genre."""
+
     class Meta:
         fields = ('name', 'slug')
         model = Genre
 
 
-class CustomCategoryField(serializers.RelatedField):
-    queryset = Category.objects.all()
+class CustomFieldMixin:
+    """Миксин для десериализации и сериализации."""
+
+    def get_queryset(self):
+        """Метод должен быть переопределен в подклассах."""
+        raise NotImplementedError(
+            'Subclasses must implement get_queryset method.')
 
     def to_internal_value(self, data):
         # Десериализация: из строки в объект
         if isinstance(data, str):
             try:
                 return self.get_queryset().get(slug=data)
-            except Category.DoesNotExist:
-                raise serializers.ValidationError('Category not found.')
+            except self.get_queryset().model.DoesNotExist:
+                raise serializers.ValidationError('Object not found.')
         return super().to_internal_value(data)
 
     def to_representation(self, value):
         # Сериализация: из объекта в словарь
-        return {
-            'name': value.name,
-            'slug': value.slug,
-        }
+        return {'name': value.name, 'slug': value.slug}
 
 
-class CustomGenreField(serializers.RelatedField):
-    queryset = Genre.objects.all()
+class CustomCategoryField(CustomFieldMixin, serializers.RelatedField):
+    def get_queryset(self):
+        return Category.objects.all()
 
-    def to_internal_value(self, data):
-        # Десериализация: из строки в объект
-        if isinstance(data, str):
-            try:
-                return self.get_queryset().get(slug=data)
-            except Genre.DoesNotExist:
-                raise serializers.ValidationError('Genre not found.')
-        return super().to_internal_value(data)
 
-    def to_representation(self, value):
-        # Сериализация: из объекта в словарь
-        return {
-            'name': value.name,
-            'slug': value.slug,
-        }
+class CustomGenreField(CustomFieldMixin, serializers.RelatedField):
+    def get_queryset(self):
+        return Genre.objects.all()
 
 
 class TitleSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Title."""
+
+    # Применяем кастомные модели, так как данные поля имеют разные типы
+    # при сериализации и десериализации.
     genre = CustomGenreField(many=True)
     category = CustomCategoryField()
+
     rating = serializers.SerializerMethodField()
 
     class Meta:
@@ -67,21 +68,17 @@ class TitleSerializer(serializers.ModelSerializer):
                   'description', 'category', 'genre')
         read_only_field = ('id', 'rating')
 
-    def get_rating(self, obj):
+    @staticmethod
+    def get_rating(obj):
+        """Получение рейтинга."""
         if obj.reviews.exists():
             return obj.reviews.aggregate(Avg('score'))['score__avg']
         return None
 
     def create(self, validated_data):
         genre_data = validated_data.pop('genre', [])
-
-        if not isinstance(genre_data, list):
-            raise serializers.ValidationError(
-                'Genre must be a list.'
-            )
-
         title = Title.objects.create(**validated_data)
-
+        # Создание связей с жанрами (ManyToMany через промежуточную модель).
         for genre in genre_data:
             TitleGenre.objects.create(
                 genre=genre, title=title)
@@ -92,39 +89,36 @@ class ReviewSerializer(serializers.ModelSerializer):
     """Сериализатор для модели Review."""
 
     author = serializers.StringRelatedField(read_only=True)
-    title = serializers.SlugRelatedField(
-        slug_field='id',
-        many=False,
-        read_only=True,
-    )
+    title = serializers.SlugRelatedField(slug_field='id',
+                                         many=False,
+                                         read_only=True, )
 
     class Meta:
         model = Review
-        fields = (
-            'id',
-            'text',
-            'author',
-            'score',
-            'pub_date',
-            'title',
-        )
+        fields = ('id', 'text', 'author', 'score', 'pub_date', 'title')
 
     def validate(self, data):
-        """Запрещает пользователям оставлять повторные отзывы"""
-        if not self.context.get('request').method == 'POST':
+        # Проверяем, что метод запроса является POST
+        if self.context['request'].method != 'POST':
             return data
-        author = self.context.get('request').user
-        title_id = self.context.get('view').kwargs.get('title_id')
+
+        # Получаем автора и идентификатор произведения из контекста запроса
+        author = self.context['request'].user
+        title_id = self.context['view'].kwargs.get('title_id')
+
+        # Проверяем, существует ли отзыв с таким автором и идентификатором
+        # произведения.
         if Review.objects.filter(author=author, title=title_id).exists():
             raise serializers.ValidationError(
-                'Вы уже оставляли отзыв на это произведение',
-            )
+                'Вы уже оставляли отзыв на это произведение')
+
         return data
 
-    def validate_score(self, value):
+    @staticmethod
+    def validate_score(value):
         """Проверка, что оценка в диапазоне от 1 до 10."""
         if not 1 <= value <= 10:
-            raise serializers.ValidationError('Оценка может быть от 1 до 10!')
+            raise serializers.ValidationError('Score must be in range 1..10.')
         return value
 
 
@@ -136,10 +130,5 @@ class CommentSerializer(serializers.ModelSerializer):
     )
 
     class Meta:
-        fields = (
-            'id',
-            'text',
-            'author',
-            'pub_date',
-        )
         model = Comment
+        fields = ('id', 'text', 'author', 'pub_date')
