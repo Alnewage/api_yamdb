4a8dce189db26ebaa1bd29ea2e2ea33f1265d270
Alexey Komissarov <al-ar-ol@yandex.ru>
2023-10-27 20:23:33 +0300

    optimize 12

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -5,9 +5,15 @@ from .views import (RegistrationViewSet, TokenViewSet, UserProfileMeView,
                     UserProfileViewSet)
 
 router_v1 = DefaultRouter()
-router_v1.register(r'auth/token', TokenViewSet, basename='token')
-router_v1.register(r'auth/signup', RegistrationViewSet, basename='registration')
-router_v1.register(r'users', UserProfileViewSet, basename='user')
+router_v1.register(r'auth/token',
+                   TokenViewSet,
+                   basename='token')
+router_v1.register(r'auth/signup',
+                   RegistrationViewSet,
+                   basename='registration')
+router_v1.register(r'users',
+                   UserProfileViewSet,
+                   basename='user')
 
 urlpatterns = [
     path('users/me/', UserProfileMeView.as_view(), name='me'),
