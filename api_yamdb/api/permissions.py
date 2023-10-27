260e4a517a4ddb7a485123a20a6c57a7b137bbaf
Alexey Komissarov <al-ar-ol@yandex.ru>
2023-10-27 17:29:06 +0300

    optimize 2

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -5,11 +5,13 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
+        # Для остальных методов, пользователь должен быть аутентифицирован
         return request.user and request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
+        # Проверяем, что пользователь является владельцем или
+        # имеет роль "admin" или "moderator".
         return any([obj.author == request.user,
-                   request.user.role == 'admin',
-                   request.user.role == 'moderator'])
+                    request.user.role in ['admin', 'moderator'], ])
