1bbb18a0fce84d26bd916369bc8c17af04c3e16f
Alexey Komissarov <al-ar-ol@yandex.ru>
2023-10-28 17:50:07 +0300

    postman optimize 9 importer_db.py

--- api_yamdb/api_yamdb/importer_db.py
+++ api_yamdb/api_yamdb/importer_db.py
@@ -1,32 +1,42 @@
 import csv
 import sqlite3
 
-
 def import_csv_to_sqlite(csv_filename, db_name, table_name):
     conn = sqlite3.connect(db_name)
     cursor = conn.cursor()
 
-    with open(csv_filename, 'r') as file:
+    with open(csv_filename, 'r', newline='', encoding='utf-8') as file:
         reader = csv.reader(file)
-        next(reader)
+        next(reader)  # Пропускаем заголовок CSV
 
         for row in reader:
-            cursor.execute(
-                f"INSERT INTO {table_name} VALUES ({', '.join(['?' for _ in row])});", row
-            )
+            placeholders = ', '.join(['?'] * len(row))
+            cursor.execute(f"INSERT INTO {table_name}"
+                           f" VALUES ({placeholders});",
+                           row)
 
     conn.commit()
     conn.close()
 
-import_csv_to_sqlite(
-    'static/data/genre.csv', 'db.sqlite3', 'reviews_genre'
-)
-import_csv_to_sqlite(
-    'static/data/category.csv', 'db.sqlite3', 'reviews_category'
-)
-import_csv_to_sqlite(
-    'static/data/titles.csv', 'db.sqlite3', 'reviews_title'
-)
-import_csv_to_sqlite(
-    'static/data/genre_title.csv', 'db.sqlite3', 'reviews_title_genre'
-)
+# Пример использования функции для каждого CSV файла
+import_csv_to_sqlite('static/data/category.csv',
+                     'db.sqlite3',
+                     'reviews_category')
+import_csv_to_sqlite('static/data/comments.csv',
+                     'db.sqlite3',
+                     'reviews_comment')
+import_csv_to_sqlite('static/data/genre.csv',
+                     'db.sqlite3',
+                     'reviews_genre')
+import_csv_to_sqlite('static/data/genre_title.csv',
+                     'db.sqlite3',
+                     'reviews_titlegenre')
+import_csv_to_sqlite('static/data/review.csv',
+                     'db.sqlite3',
+                     'reviews_review')
+import_csv_to_sqlite('static/data/titles.csv',
+                     'db.sqlite3',
+                     'reviews_title')
+import_csv_to_sqlite('static/data/users.csv',
+                     'db.sqlite3',
+                     'users_myuser')
