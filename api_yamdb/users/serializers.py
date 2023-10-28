dcb7061a45c97e6838db42ec7d438e7e6105a879
Alexey Komissarov <al-ar-ol@yandex.ru>
2023-10-28 16:58:24 +0300

    postman optimize 7 327 tests passed

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -79,7 +79,7 @@ class UserProfileSerializer(ValidateUsernameMixin,
     class Meta:
         model = User
         fields = (
-            'username', 'email', 'first_name', 'last_name', 'bio', 'role',)
+            'username', 'email', 'role', 'first_name', 'last_name', 'bio')
 
     def validate_role(self, value):
         user = self.context['request'].user
